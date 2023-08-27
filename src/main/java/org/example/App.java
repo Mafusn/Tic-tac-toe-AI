90f4082dc826e023b22521ac6fe3fa31f4fdc0da
Mafusn <Magnus.peetz.holt@gmail.com>
2023-08-27 16:29:34 +0200

    Update model3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,16 +8,16 @@ public class App
         Board board = new Board();
         AiPlayer aiPlayer0 = new AiPlayer('O', 1);
         AiPlayer aiPlayer1 = new AiPlayer('O', 1);
-        AiPlayer aiPlayer2 = new AiPlayer('O', 1);
-        AiPlayer aiPlayer3 = new AiPlayer('X', 3);
-        AiPlayer aiPlayer4 = new AiPlayer('X', 4);
+        AiPlayer aiPlayer2 = new AiPlayer('O', 2);
+        AiPlayer aiPlayer3 = new AiPlayer('X', 3); // Overpowered
 
-        aiPlayer4.getNeuralNetworkPlayer().trainSelfPlayToBeFirst(100000, 4);
 
-        int numGames= 1000;
+        aiPlayer3.getNeuralNetworkPlayer().trainSelfPlayToBeSecond(250000, 3);
+
+        int numGames= 100;
         for (int i = 0; i < numGames; i++) {
             board.clearBoard();
-            board.startOnlyAiGameWithoutDisplaying(aiPlayer3, aiPlayer1, 0.1);
+            board.startOnlyAiGameWithoutDisplaying(aiPlayer3, aiPlayer0, 0.1);
         }
 
         System.out.println("X wins: " + board.getXWins() + "\nO wins: " + board.getOWins() + "\nTies: " + board.getTies());
@@ -25,7 +25,7 @@ public class App
         Board board1 = new Board();
         for (int i = 0; i < numGames; i++) {
             board1.clearBoard();
-            board1.startOnlyAiGameWithoutDisplaying(aiPlayer4, aiPlayer1, 0.1);
+            board1.startOnlyAiGameWithoutDisplaying(aiPlayer0, aiPlayer3, 0.1);
         }
 
         System.out.println("X wins: " + board1.getXWins() + "\nO wins: " + board1.getOWins() + "\nTies: " + board1.getTies());
