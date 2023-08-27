6bbb44578bd4b8b07c964465f3f02abb2d0368c9
Mafusn <Magnus.peetz.holt@gmail.com>
2023-08-27 15:15:19 +0200

    update model4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,17 +6,28 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
         Board board = new Board();
-        AiPlayer aiPlayerX = new AiPlayer('X', 3);
-        AiPlayer aiPlayerO = new AiPlayer('O', 1);
+        AiPlayer aiPlayer0 = new AiPlayer('O', 1);
+        AiPlayer aiPlayer1 = new AiPlayer('O', 1);
+        AiPlayer aiPlayer2 = new AiPlayer('O', 1);
+        AiPlayer aiPlayer3 = new AiPlayer('X', 3);
+        AiPlayer aiPlayer4 = new AiPlayer('X', 4);
 
-        aiPlayerX.getNeuralNetworkPlayer().trainSelfPlayToBeSecond(100000, 3);
+        aiPlayer4.getNeuralNetworkPlayer().trainSelfPlayToBeFirst(100000, 4);
 
-        int numGames= 100;
+        int numGames= 1000;
         for (int i = 0; i < numGames; i++) {
             board.clearBoard();
-            board.startOnlyAiGameWithoutDisplaying(aiPlayerX, aiPlayerO, 0.1);
+            board.startOnlyAiGameWithoutDisplaying(aiPlayer3, aiPlayer1, 0.1);
         }
 
         System.out.println("X wins: " + board.getXWins() + "\nO wins: " + board.getOWins() + "\nTies: " + board.getTies());
+
+        Board board1 = new Board();
+        for (int i = 0; i < numGames; i++) {
+            board1.clearBoard();
+            board1.startOnlyAiGameWithoutDisplaying(aiPlayer4, aiPlayer1, 0.1);
+        }
+
+        System.out.println("X wins: " + board1.getXWins() + "\nO wins: " + board1.getOWins() + "\nTies: " + board1.getTies());
     }
 }
