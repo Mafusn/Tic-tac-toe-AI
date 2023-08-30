247c674a4caa7b303f9060e3d9c617be009a1dde
Mafusn <Magnus.peetz.holt@gmail.com>
2023-08-30 19:29:04 +0200

    New training model
    2 hidden layers -32 hidden nodes in each

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,44 +8,33 @@ public class App
         Board board = new Board();
 
         // Random mod random = 59% winrate for startende spiller
+        // Træningsinterval på 1000 er bedst
+        // 64 Hidden nodes har virket bedst indtil videre
 
-        AiPlayer aiPlayer0 = new AiPlayer(0);
-        AiPlayer aiPlayer1 = new AiPlayer(1); // Bedst trænet med at 'O' bliver sat random - 0.01 epsilon omkring 75% winrate
-        AiPlayer aiPlayer2 = new AiPlayer(2); // trænet på samme måde som 3 vist - 0.01 epsilon omkring 75% winrate
-        AiPlayer aiPlayer3 = new AiPlayer(3); // 0.01 epsilon omkring 76% winrate
-        AiPlayer aiPlayer4 = new AiPlayer(4);
-        AiPlayer aiPlayer5 = new AiPlayer(5);
-        AiPlayer aiPlayer6 = new AiPlayer(6);
-        AiPlayer aiPlayer7 = new AiPlayer(7);
-        AiPlayer aiPlayer8 = new AiPlayer(8);
-        AiPlayer aiPlayer9 = new AiPlayer(9);
-        AiPlayer aiPlayer10 = new AiPlayer(10);
-        AiPlayer aiPlayer11 = new AiPlayer(11);
-        AiPlayer aiPlayer12 = new AiPlayer(12);
-        AiPlayer aiPlayer14 = new AiPlayer(14);
-        AiPlayer aiPlayer15 = new AiPlayer(15);
-        AiPlayer aiPlayer16 = new AiPlayer(16);
-        AiPlayer aiPlayer17 = new AiPlayer(17);
-        AiPlayer aiPlayer18 = new AiPlayer(18);
+        AiPlayer aiPlayer0 = new AiPlayer(0); // 84% win rate - Iterations: 500.000, Training interval: 1000, started - 2 hidden layers - See ConfigPictures/Model0 for configuration
+        AiPlayer aiPlayer1 = new AiPlayer(1); //
+        AiPlayer aiPlayer2 = new AiPlayer(2); //
+        AiPlayer aiPlayer3 = new AiPlayer(3); //
+        AiPlayer aiPlayer4 = new AiPlayer(4); //
+        AiPlayer aiPlayer5 = new AiPlayer(5); //
+        AiPlayer aiPlayer6 = new AiPlayer(6); //
+        AiPlayer aiPlayer7 = new AiPlayer(7); //
+        AiPlayer aiPlayer8 = new AiPlayer(8); //
+        AiPlayer aiPlayer9 = new AiPlayer(9); //
 
-        aiPlayer4.getNeuralNetworkPlayer().trainSelfPlay(100000, 100, 'X');
-        aiPlayer5.getNeuralNetworkPlayer().trainSelfPlay(100000, 1000, 'X');
-        aiPlayer6.getNeuralNetworkPlayer().trainSelfPlay(250000, 100, 'X');
-        aiPlayer7.getNeuralNetworkPlayer().trainSelfPlay(250000, 1000, 'X');
-        aiPlayer8.getNeuralNetworkPlayer().trainSelfPlay(500000, 100, 'X');
-        aiPlayer9.getNeuralNetworkPlayer().trainSelfPlay(500000, 1000, 'X');
-        aiPlayer10.getNeuralNetworkPlayer().trainSelfPlay(1000000, 100, 'X');
-        aiPlayer11.getNeuralNetworkPlayer().trainSelfPlay(1000000, 1000, 'X');
-
-        /*aiPlayer15.getNeuralNetworkPlayer().trainSelfPlay(500000, 100, 'X');
-        aiPlayer16.getNeuralNetworkPlayer().trainSelfPlay(500000, 1000, 'X');
-        aiPlayer17.getNeuralNetworkPlayer().trainSelfPlay(500000, 100, 'X');
-        aiPlayer18.getNeuralNetworkPlayer().trainSelfPlay(500000, 1000, 'X');*/
+        aiPlayer1.getNeuralNetworkPlayer().trainSelfPlay(100000, 100, 'X');
+        aiPlayer2.getNeuralNetworkPlayer().trainSelfPlay(100000, 1000, 'X');
+        aiPlayer3.getNeuralNetworkPlayer().trainSelfPlay(250000, 100, 'X');
+        aiPlayer4.getNeuralNetworkPlayer().trainSelfPlay(250000, 1000, 'X');
+        aiPlayer5.getNeuralNetworkPlayer().trainSelfPlay(500000, 100, 'X');
+        aiPlayer6.getNeuralNetworkPlayer().trainSelfPlay(500000, 1000, 'X');
+        aiPlayer7.getNeuralNetworkPlayer().trainSelfPlay(1000000, 100, 'X');
+        aiPlayer8.getNeuralNetworkPlayer().trainSelfPlay(1000000, 1000, 'X');
 
         int numGames= 10000;
         for (int i = 0; i < numGames; i++) {
             board.clearBoard();
-            board.startOnlyAiGameWithoutDisplaying(aiPlayer4, 0.01);
+            board.startOnlyAiGameWithoutDisplaying(aiPlayer1, 0.01);
             //board.startOnlyAiGame(aiPlayer1, aiPlayer2, 0.1);
         }
         board.showWins(numGames);
@@ -53,60 +42,60 @@ public class App
         Board board1 = new Board();
         for (int i = 0; i < numGames; i++) {
             board1.clearBoard();
-            board1.startOnlyAiGameWithoutDisplaying(aiPlayer5, 0.01);
+            board1.startOnlyAiGameWithoutDisplaying(aiPlayer2, 0.01);
         }
         board1.showWins(numGames);
 
         Board board2 = new Board();
         for (int i = 0; i < numGames; i++) {
             board2.clearBoard();
-            board2.startOnlyAiGameWithoutDisplaying(aiPlayer6, 0.01);
+            board2.startOnlyAiGameWithoutDisplaying(aiPlayer3, 0.01);
         }
         board2.showWins(numGames);
 
         Board board3 = new Board();
         for (int i = 0; i < numGames; i++) {
             board3.clearBoard();
-            board3.startOnlyAiGameWithoutDisplaying(aiPlayer7, 0.01);
+            board3.startOnlyAiGameWithoutDisplaying(aiPlayer4, 0.01);
         }
         board3.showWins(numGames);
 
         Board board4 = new Board();
         for (int i = 0; i < numGames; i++) {
             board4.clearBoard();
-            board4.startOnlyAiGameWithoutDisplaying(aiPlayer8, 0.01);
+            board4.startOnlyAiGameWithoutDisplaying(aiPlayer5, 0.01);
         }
         board4.showWins(numGames);
 
         Board board5 = new Board();
         for (int i = 0; i < numGames; i++) {
             board5.clearBoard();
-            board5.startOnlyAiGameWithoutDisplaying(aiPlayer9, 0.01);
+            board5.startOnlyAiGameWithoutDisplaying(aiPlayer6, 0.01);
         }
         board5.showWins(numGames);
 
         Board board6 = new Board();
         for (int i = 0; i < numGames; i++) {
             board6.clearBoard();
-            board6.startOnlyAiGameWithoutDisplaying(aiPlayer10, 0.01);
+            board6.startOnlyAiGameWithoutDisplaying(aiPlayer7, 0.01);
         }
         board6.showWins(numGames);
 
         Board board7 = new Board();
         for (int i = 0; i < numGames; i++) {
             board7.clearBoard();
-            board7.startOnlyAiGameWithoutDisplaying(aiPlayer11, 0.01);
+            board7.startOnlyAiGameWithoutDisplaying(aiPlayer8, 0.01);
         }
         board7.showWins(numGames);
 
-        /*Board board8 = new Board();
+        Board board8 = new Board();
         for (int i = 0; i < numGames; i++) {
             board8.clearBoard();
-            board8.startOnlyAiGameWithoutDisplaying(aiPlayer10, 0.1);
+            board8.startOnlyAiGameWithoutDisplaying(aiPlayer0, 0.01);
         }
         board8.showWins(numGames);
 
-        Board board9 = new Board();
+        /*Board board9 = new Board();
         for (int i = 0; i < numGames; i++) {
             board9.clearBoard();
             board9.startOnlyAiGameWithoutDisplaying(aiPlayer11, 0.1);
